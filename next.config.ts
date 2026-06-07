37f4c3e59169b88adc9a81003f847fb2f1921940
Z User <z@container>
2026-06-07 14:24:16 +0000

    fix: use BUILD_DIR env var for zero-downtime staging builds

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
+  distDir: process.env.BUILD_DIR || "./.next",
   typescript: {
     ignoreBuildErrors: true,
   },
